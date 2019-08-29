81124f680dd13c138f306b969db099c3cb10a0a2
ADelgadillo314 <adelgadillo314@ucla.edu>
2019-08-28 17:15:49 -0700

    Updates

---
+++ precache-manifest.318781584eb6132ac1fc16bd93de66e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8267f16086abe9d4bfb1e5ce3d68fe92",
+    "url": "/CaptureGame/index.html"
+  },
+  {
+    "revision": "1dc46cedd57ed23b3329",
+    "url": "/CaptureGame/static/css/main.a7ff2cc6.chunk.css"
+  },
+  {
+    "revision": "93d3df02b88d0a0495d9",
+    "url": "/CaptureGame/static/js/2.1221463b.chunk.js"
+  },
+  {
+    "revision": "1dc46cedd57ed23b3329",
+    "url": "/CaptureGame/static/js/main.c6212410.chunk.js"
+  },
+  {
+    "revision": "cb08d8aec7f6209e27af",
+    "url": "/CaptureGame/static/js/runtime~main.863493ec.js"
+  }
+]);
